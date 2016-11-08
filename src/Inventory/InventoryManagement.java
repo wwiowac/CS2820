53cc4dcf0fe06776ce498efb7089d9688e174a5c
kdenhartog <kyle-denhartog@uiowa.edu>
2016-11-07 22:38:51 -0600

    This is the beginning stages of Inventory. I am still looking for a good .csv file for inventory. If I don't find one soon, I plan to make one up or borrow one from another group.

---
+++ src/Inventory/InventoryManagement.java
@@ -0,0 +1,11 @@
+package Inventory;
+
+import java.util.HashSet;
+
+public class InventoryManagement {
+
+    public HashSet<InventoryItem> currentInventory;
+
+
+
+}
---
+++ src/Inventory/MockShelf.java
@@ -0,0 +1,13 @@
+package Inventory;
+
+public class MockShelf {
+
+    private int shelfId;
+
+    public MockShelf(int shelfId){
+        this.shelfId = shelfId;
+    }
+    public int getShelfId() {
+        return shelfId;
+    }
+}
