033dc2cbe667acf292a0bcc009e7a4648c7cb753
Guth <jrguth@uiowa.edu>
2016-11-16 02:01:29 -0600

    Fixed and updated Belt.java, completed Package.java

--- src/Belt.java
+++ src/Belt.java
@@ -1,242 +1,287 @@
 /*
 *@author Jacob Guth
 *
-* Rough draft of Belt.java 11/8/2016
+* Rough draft of Belt.java 
 *
-* NOTE: Further implementation will require a definition of a Package, an object that contains multiple 
-* InventoryItem references
+* Updated 11/16/2106
 *
-* TODO: implement initializeBelt(), a way to keep track of and increment time
+* TODO: implement EventConsumerInterface
 */
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.awt.*;
 
-import java.util*;
 public class Belt {
-	
-	private Floor floor;
-	
-	
-	//picker and packer locations
-	private Point begin;	
-	private Point end;			
-		
-	private Integer currentTime;
-	private Integer length;
-	private Integer numCells;
-	
-	//Maps each cell id to its Cell object
-	private HashMap<int, Cell> cells;
-	
-	//Maps each package id to the Cell it occupies
-	private HashMap<int, Cell> packages;
-	
-	//Maps Belt cell indices to a Point location
-	private ArrayList<Point> indexLocations;
-	
-	//booleans controllers for belt movement
-	private boolean isMoving;
-	private boolean canMove;
-	
-	/*
-	* constructor -  initializes a belt with a designated Floor layout, begin/end points, and number of cells
-	* 
-	*/
-	public Belt(Floor floor, Point begin, Point end, int numCells){
-		
-		this.floor = floor;
-		this.begin = begin;
-		this.end = end;
-		this.numCells = numCells;
-		
-		currentTime = 0;
-		isMoving = false;
-		canMove = true;
-		length = end.getY() - begin.getY();
-		
-		cells = new HashMap<>()
-		packages = new HashMap<>();
-		
-		initializeBelt();
-		numCells = cells.length();
-	}
-	
-	/*
-	* description: if the belt can move at currentTime, update the locations of each cell in the belt
-	*/
-	private void move(){
-		if(canMove){
-			updateLocations();
-			currentTime++;
-		}	
-	}
-	
-	/*
-	* description: starts/stops belt movement
-	*/
-	public void resume(){
-		canMove = true;
-	}
 
-	public void stop(){
-		canMove = false;
-	}
-	
-	
-	/*
-	*@param package: an ArrayList of inventory items that represents a package (bin, box, etc.)
-	*
-	* description: adds/removes a package from the specified cell on the belt
-	*/
-	public void addPackage(Package package, Cell cell){	
-	}
-	
-	public void removePackage(Package package, Cell cell){
-	}
-	
-	
-	/*
-	* @param id: a cell id
-	* @return: Cell object associate with @param id
-	*/
-	public Cell getCell(int id){
-		return cells.get(id);
-	}
-	
-	
-	/*
-	*@param package: A package of InventoryItems (bin, box, order, etc.)
-	*@return: Point object associated with the cell @param package currently occupies
-	*/
-	public Point getPackageLocation(Package package){
-		return packages.get(package.id).getLocation();
-	}
-	
-	
-	/*
-	* description: updates the current status of the Belt to the Floor
-	*
-	* NOTE: further implementation I believe will require implementation of an updateStatus in
-	* Floor.java
-	*
-	*/
-	public void updateStatus(){
-	}
-	
-	/*
-	*description: increments the location of each Cell on the belt
-	*/
-	private void updateLocations(){
-		for(Cell cell : cells.values() ){
-			cell.incrementIndex();
-		}
-	}
-	
-	
-	/*
-	* description: This method will initialize the belt separated by cells. It will also initialize  
-	* indexLocations representing the location of each index. Will complete once 
-	* we decide on the length of the belt, the number of cells we want, etc. 
-	*/
-	private void initializeBelt(){	
-	}
-	
-	/*
-	* description: increment the time
-	*/
-	private void tick(){
-		this.currentTime++;
-	}
-	
-	/*
-	*Belt is comprised of Cells, each with an index indicating its location on the belt and
-	*an id. 
-	*
-	* description: class Cell has methods that identify and locate the packages they are occupied with
-	*/
-	private class Cell{
-		
-		private Point location;
-		
-		private int index;
-		private int id;
-		private int width;
-		private int height;
-		private boolean occupied;
-		private Package package;
-		
-		/*
-		* Constructor: initializes a cell with a designated width, height, id, and location
-		*
-		*/
-		public Cell(int width, int height, int id, Point location){
-			this.width = width;
-			this.id = id;
-			this.location = location;
-			occupied = false;
-			package = null;
-			
-			cells.put(id, this);	
-		}
-		
-		/*
-		* @param package: Package to be added to this Cell
-		*/
-		public void addPackage(Package package){
-			this.package = package;
-			occupied = true;
-		}
-		
-		/*
-		* description: removes and returns the package from this cell
-		*/
-		public Package removePackage(){
-			Package temp = this.package;
-			this.package = null;
-			occupied = false;
-			return temp;
-		}
-		
-		public boolean isOccupied(
-			return occupied;
-		)
-		
-		/*
-		* @return: returns the package occupying this cell, if there is one; Prints  a message otherwise
-		*/
-		public Package getPackage(){
-			try{
-				return package;
-			}catch(Exception e){
-				System.out.println("Cell "+id+"does not contain a package");
-			}
-		}
-		
-		/*
-		* @return: the location of this cell
-		*/
-		public Point getLocation(){
-			return location;
-		}
-		
-		/*
-		* description: increment this cell's index by one; if it's at the end of the belt, move it to
-		* the beginning
-		*/
-		public void incrementIndex(){
-			if(index == numCells){
-				index = 0;
-			}else{
-				index++;
-			}
-			updateLocation();
-		}
-		
-		
-		 /*
-		 * description: updates the location of this cell
-		 */
-		public void updateLocation(){
-			this.location = indexLocations.get(this.index);
-		}
-	}
-	
+    private Floor floor;
+    
+    //picker and packer locations
+    private final Point begin;	
+    private final Point end;			
+
+    private Integer currentTime;
+
+    private final Integer BELT_LENGTH;
+    private final Integer BELT_WIDTH;
+    private final Integer NUM_CELLS;
+
+    //Maps each beltCell id to its object
+    private HashMap<String, BeltCell> cells;
+    //Maps the ID of each package on the belt to its object
+    private HashMap<String, Package> packages;
+    //Maps each package on the belt to the beltCell it occupies
+    private HashMap<String, BeltCell> packageCells;
+    //Maps each index on the belt to its point location
+    private HashMap<Integer, Point> indexLocations;
+    
+
+    //booleans controller for belt movement
+    private boolean canMove;
+
+    /*
+    * constructor -  initializes a belt with a designated Floor layout
+    * 
+    */
+    public Belt(Floor floor){
+
+        this.floor = floor;
+        
+        //
+        this.begin = new Point(0,100);
+        this.end = new Point(100,0);
+
+        currentTime = 0;
+        canMove = true;
+
+        //Belt length depends on begin and end point. Belt width set to 20 (as shown in FloorPlan.png), and number of cells set to 50
+        BELT_LENGTH = (int) Math.abs(end.getY() - begin.getY());
+        BELT_WIDTH = 20;
+        NUM_CELLS = 50;
+
+
+        cells = new HashMap<>();
+        packages = new HashMap<>();
+        packageCells = new HashMap<>();
+
+        initializeBelt();
+    }
+
+    /*
+    * description: if the belt can move at currentTime, update the locations of each cell in the belt
+    */
+    private void move(){
+        if(canMove){
+            updateLocations();
+            currentTime++;
+        }	
+    }
+
+    /*
+    * description: starts/stops belt movement
+    */
+    public void resume(){
+        canMove = true;
+    }
+
+    public void stop(){
+        canMove = false;
+    }
+
+
+    /*
+    *@param inventoryPackage: a Package of InventoryItems
+    *
+    * description: adds a Package to the specified beltCell on the belt
+    */
+    public void addPackage(Package inventoryPackage, String cell_id){
+        String packageID = inventoryPackage.getID();
+        
+        cells.get(cell_id).addPackage(inventoryPackage);
+        packages.put(packageID, inventoryPackage);
+        packageCells.put(packageID, cells.get(cell_id));
+    }
+    
+    /*
+    * @param inventoryPackage: a Package of InventoryItems
+    * 
+    * description: removes and returns the Package specified by packageID from the belt
+    */
+    public Package removePackage(String packageID){
+
+        packages.remove(packageID);
+        packageCells.remove(packageID);
+        
+        return packageCells.get(packageID).removePackage();
+    }
+
+
+    /*
+    * @param id: a cell id
+    * @return: Cell object associate with @param id
+    */
+    public BeltCell getCell(String id){
+        return cells.get(id);
+    }
+
+
+    /*
+    *@param packageID: string ID of a package 
+    *@return: Point where @param inventoryPackage resides
+    */
+    public Point getPackageLocation(String packageID){
+        if(!packageCells.containsKey(packageID)){
+            System.out.println("Package "+packageID+"not found on the belt");
+            return null;
+        }else{
+            return packageCells.get(packageID).getLocation();
+        }
+    }
+
+
+    /*
+    * description: updates the current status of the Belt to the Floor
+    *
+    * NOTE: further implementation I believe will require implementation of an updateStatus in
+    * Floor.java
+    *
+    */
+    public void updateStatus(){
+    }
+
+    /*
+    *description: increments the location of each beltCell on the belt
+    */
+    private void updateLocations(){
+        for(BeltCell beltCell : cells.values() ){
+            beltCell.incrementIndex();
+        }
+    }
+
+
+    /*
+    * description: This method will initialize the belt separated by beltCells. It will also initialize  
+    * indexLocations representing the location of each index. Will complete once 
+    * we decide on the length of the belt, the number of cells we want, etc. 
+    */
+    private void initializeBelt(){
+        
+        int cellHeight = BELT_LENGTH/NUM_CELLS;
+        
+        //define indexLocations with keys NUM_CELLS (50) indices mapped to values top-left corner Point locations
+        for(int yLoc = 0, index = 0; yLoc < BELT_LENGTH-cellHeight; yLoc+= cellHeight, index++){
+            indexLocations.put(index, new Point(0,yLoc));
+        }
+        
+        //define cells with keys ID (same as starting index) mapped to the newly created cell
+        for(int i = 0; i<=NUM_CELLS; i++){
+            Point location = indexLocations.get(i);
+            String ID = Integer.toString(i);
+            BeltCell cell = new BeltCell(cellHeight, i, location, ID);
+            cells.put(ID, cell);
+        }
+    }
+
+    /*
+    *Belt is comprised of beltCells, each with an index indicating its location on the belt and
+    *an id. 
+    *
+    * description: class Cell has methods that identify and locate the packages they are occupied with
+    */
+    private class BeltCell {
+        
+        private Point location;
+        private int index;
+  
+        private final String ID;
+
+        private final int width;
+        private final int height;
+
+        private boolean occupied;
+
+        private Package inventoryPackage;
+
+        /*
+        * Constructor: initializes a cell with a designated height, initial index, initial location, and ID
+        *
+        */
+        public BeltCell(int height, int index, Point location, String ID){
+            
+            this.location = location;
+            this.index = index;
+            this.height = height;
+            this.ID = ID;
+            this.width = BELT_WIDTH;
+            this.inventoryPackage = null;
+            occupied = false;
+        }
+
+        public String getID(){
+            return ID;
+        }
+        
+        public Point getLocation(){
+            return location;
+        }
+        
+        public int getWidth(){
+            return width;
+        }
+        
+        public int getHeight(){
+            return height;
+        }
+        /*
+        * @param package: Package to be added to this Cell
+        */
+        public void addPackage(Package inventoryPackage){
+            this.inventoryPackage = inventoryPackage;
+            occupied = true;
+        }
+
+        /*
+        * description: removes and returns the package from this cell
+        */
+        public Package removePackage(){
+            Package temp = inventoryPackage;
+            inventoryPackage = null;
+            occupied = false;
+            return temp;
+        }
+
+        public boolean isOccupied(){
+            return occupied;
+        }
+
+        /*
+        * @return: returns the package occupying this cell, if there is one; Prints  a message otherwise
+        */
+        public Package getPackage(){
+            try{
+                return inventoryPackage;
+            }catch(Exception e){
+                System.out.println("Cell "+ID+"does not contain a package");
+            }
+            return null;
+        }
+        /*
+        * description: increment this cell's index by one; if it's at the end of the belt, move it to
+        * the beginning
+        */
+        public void incrementIndex(){
+            if(index == NUM_CELLS -1){
+                index = 0;
+            }else{
+                index++;
+            }
+            updateLocation();
+        }
+
+         /*
+         * description: updates the location of this cell
+         */
+        public void updateLocation(){
+            location = indexLocations.get(index);  
+        }
+    }
 }
