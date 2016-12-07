ceed6d5c7cbcbacbbb90339d49bb84c983503d68
jrguth <jacob-guth@uiowa.edu>
2016-12-06 21:53:01 -0600

    Implemented Packer

--- src/production/Belt.java
+++ src/production/Belt.java
@@ -1,35 +1,38 @@
 package production;
 
 import java.awt.*;
-import java.util.ArrayList;
 
 /**
- * Created by jacobguth on 12/5/16.
+ * @author Jacob Guth
+ * @author Jacob Roschen
+ * @author Wesley
  */
 public class Belt implements EventConsumer{
 
     private Master master;
     private Floor floor;
-
     private Bin[] cells = new Bin[30];
 
     private boolean celloccupied = false;
 
-    private boolean canMove;
-    private int NUM_CELLS;
-
+    /**
+     * @constructor: initializes a belt with 30 cells
+     * @param master
+     * @param floor
+     */
     public Belt(Master master, Floor floor){
 
         this.master = master;
         this.floor = floor;
-
-        canMove = true;
 
         for(int i=0; i<cells.length; i++){
             floor.updateItemAt(new Point(0,i), Cell.Type.BELT);
         }
     }
 
+    /**
+     * @description: increments the contents of the cells on the belt, changing the cell type accordingly
+     */
     private void move(){
         celloccupied = false;
         for(int i=1; i<cells.length; i++){
@@ -42,8 +45,18 @@ public class Belt implements EventConsumer{
         }
         cells[cells.length-1] = null;
         floor.updateItemAt(new Point (0, cells.length-1), Cell.Type.BELT);
+        //If a bin has reached the packer, schedule an OrderPacked event
+        if(cells[11] != null){
+            Event event = new Event(new Task(Task.TaskType.OrderPacked, cells[11].getOrder()), master.packer);
+            master.scheduleEvent(event);
+        }
     }
 
+    /**
+     * @description: task handler for moving the belt and adding a bin to the belt
+     * @param task
+     * @param event
+     */
     @Override
     public void handleTaskEvent(Task task, Event event) {
         switch (task.type){
@@ -57,22 +70,16 @@ public class Belt implements EventConsumer{
             case AddBinToBelt:
                 addBin(task.bin, task.location.y);
                 master.scheduleEvent(event,1);
+                System.out.println("New bin created for picker");
                 break;
         }
     }
-    /**
-     * @description: starts belt movement
-     */
-    public void resume(){
-        canMove = true;
-    }
-    /**
-     * @description: stops belt movement
-     */
-    public void stop(){
-        canMove = false;
-    }
 
+    /**
+     *
+     * @param bin: bin to be added to the belt
+     * @param index: index of the cell the bin is being added to
+     */
     public void addBin(Bin bin, int index){
         if (!celloccupied) {
             Event spawnedEvent = new Event(new Task(Task.TaskType.MoveBelt), this);
@@ -82,5 +89,4 @@ public class Belt implements EventConsumer{
         cells[index] = bin;
         floor.updateItemAt(new Point(0,index), Cell.Type.BINONBELT);
     }
-
 }
